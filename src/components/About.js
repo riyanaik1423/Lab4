fbbce4b83266220332ab945cd562110bae400790
riyanaik1423 <riya.naik1@mitwpu.edu.in>
2025-10-28 23:14:23 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h2>About This App</h2>
+      <p>A simple Task Manager using React Router.</p>
+    </div>
+  );
+}
